4f1a6c180399245520fcf2995c2c475f079b41bb
Hamza <hamzaalbouchi9@gmail.com>
2026-04-01 15:33:44 +0200

    add key for beta use - fix

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication(scanBasePackages = {"com.example.demo", "com.tiramisu"})
-@EntityScan(basePackages = {"com.tiramisu.entity"})
-@EnableJpaRepositories(basePackages = {"com.tiramisu.entity"})
+@EntityScan(basePackages = {"com.tiramisu.entity", "com.example.demo.invite"})
+@EnableJpaRepositories(basePackages = {"com.tiramisu.entity", "com.example.demo.invite"})
 public class DemoApplication {
 
     public static void main(String[] args) {
